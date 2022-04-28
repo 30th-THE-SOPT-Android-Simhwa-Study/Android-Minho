0b0a0c668ddd8ac881c13b151398240b91e24a85
minho <my_alsgh@naver.com>
2022-04-29 03:03:10 +0900

    Apply Review and Comment

--- app/src/main/java/com/sopt/androidstudy/data/model/UserData.kt
+++ app/src/main/java/com/sopt/androidstudy/data/model/UserData.kt
@@ -2,26 +2,7 @@ package com.sopt.androidstudy.data.model
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
-data class UserData(val uid: String?, val password:String?):Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString(),
-        parcel.readString()
-    ) {
-    }
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(uid)
-        parcel.writeString(password)
-    }
-    override fun describeContents(): Int {
-        return 0
-    }
-    companion object CREATOR : Parcelable.Creator<UserData> {
-        override fun createFromParcel(parcel: Parcel): UserData {
-            return UserData(parcel)
-        }
-        override fun newArray(size: Int): Array<UserData?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+@Parcelize
+data class UserData(val uid: String?, val password:String?):Parcelable
