24b05d7927007ea5d403f1f5878c5f1fce77fb13
MinHo <my_alsgh@naver.com>
2022-06-22 09:34:01 +0900

    Update app/src/main/java/com/sopt/androidstudy/data/remote/github/models/ResponseUser.kt
    반영!
    
    Co-authored-by: choiyoungjin <cyjin6789@gmail.com>

--- app/src/main/java/com/sopt/androidstudy/data/remote/github/models/ResponseUser.kt
+++ app/src/main/java/com/sopt/androidstudy/data/remote/github/models/ResponseUser.kt
@@ -1,7 +1,7 @@
 package com.sopt.androidstudy.data.remote.github.models
 
 data class ResponseUser(
-    val avatar_url: String?="",
+    val avatar_url: String? = "",
     val login: String,
     val name: String,
     val location: String
