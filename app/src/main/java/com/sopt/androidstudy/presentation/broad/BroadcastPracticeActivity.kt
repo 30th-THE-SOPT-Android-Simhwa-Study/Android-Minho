f043c2f789ac4395fd4fd0987b532369fde848ca
minho <my_alsgh@naver.com>
2022-09-02 20:42:37 +0900

    [Feat] Complete BroadCast Receiver Practice

--- app/src/main/java/com/sopt/androidstudy/presentation/broad/BroadcastPracticeActivity.kt
+++ app/src/main/java/com/sopt/androidstudy/presentation/broad/BroadcastPracticeActivity.kt
@@ -2,15 +2,14 @@ package com.sopt.androidstudy.presentation.broad
 
 import android.Manifest
 import android.content.Intent
-import android.content.IntentFilter
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.provider.Telephony
+import android.telephony.SmsManager
+import android.text.Editable
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.sopt.androidstudy.SMSReceiver
 import com.sopt.androidstudy.databinding.ActivityBroadcastPracticeBinding
 
 
@@ -24,15 +23,17 @@ class BroadcastPracticeActivity : AppCompatActivity() {
         binding = ActivityBroadcastPracticeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         requirePerms()
-        intent = getIntent()
         processedIntent(intent)
         clickEvent()
     }
+
     private fun requirePerms() {
-        val permissions = arrayOf<String>(Manifest.permission.RECEIVE_SMS)
+        val permissions = arrayOf(Manifest.permission.RECEIVE_SMS, Manifest.permission.SEND_SMS)
         val permissionCheck =
             ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)
-        if (permissionCheck == PackageManager.PERMISSION_DENIED) {
+        val permissionCheck2 =
+            ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+        if (permissionCheck == PackageManager.PERMISSION_DENIED || permissionCheck2 == PackageManager.PERMISSION_DENIED) {
             ActivityCompat.requestPermissions(this, permissions, 1)
         }
     }
@@ -54,23 +55,29 @@ class BroadcastPracticeActivity : AppCompatActivity() {
 
     //받은 문자 - 문자열 set
     private fun processedIntent(intent: Intent?) {
-        val str = intent?.getStringExtra("data")
-        binding.etContent.setText(str)
+        val (phoneNumber, message) = (intent?.getStringExtra("data") ?: " ").split(" ")
+        with(binding) {
+            etContent.setText(message)
+            etPhone.setText(phoneNumber)
+        }
     }
 
     private fun clickEvent() {
         binding.btnSend.setOnClickListener {
-            Intent().also { intent ->
-                intent.action = "com.sopt.androidstudy"
-                intent.putExtra("data", "notiBroadCast")
-                sendBroadcast(intent)
-            }
+            val smsManager: SmsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage(
+                binding.etPhone.text.toString(),
+                null,
+                binding.etContent.text.toString(),
+                null,
+                null
+            )
         }
     }
 
     //앱이 종료되지 않은 상태에서 메시지를 받을경우 Receiver 에서 startActivity에 의해 onNewIntent가 실행됨.
-    override fun onNewIntent(intent: Intent?) {
-        Log.d("Message New", intent.toString())
+    override fun onNewIntent(intent: Intent) {
+        Log.d("Message New", intent.getStringExtra("data").toString())
         processedIntent(intent)
         super.onNewIntent(intent)
     }
