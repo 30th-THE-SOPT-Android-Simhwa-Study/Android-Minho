1730f7d7c5cd6543ff41a2e3ba6e1aee978728b6
minho <my_alsgh@naver.com>
2022-09-27 00:45:08 +0900

    [Feat] Complete Load Prev ChattingList

--- app/src/main/java/com/sopt/androidstudy/presentation/chatting/SocketChattingActivity.kt
+++ app/src/main/java/com/sopt/androidstudy/presentation/chatting/SocketChattingActivity.kt
@@ -2,16 +2,61 @@ package com.sopt.androidstudy.presentation.chatting
 
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.sopt.androidstudy.presentation.base.BaseActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.sopt.androidstudy.R
 import com.sopt.androidstudy.databinding.ActivitySocketChattingBinding
+import com.sopt.androidstudy.domain.util.ApiResult
+import com.sopt.androidstudy.presentation.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import timber.log.Timber
 
 @AndroidEntryPoint
-class SocketChattingActivity : BaseActivity<ActivitySocketChattingBinding>(R.layout.activity_socket_chatting) {
+class SocketChattingActivity :
+    BaseActivity<ActivitySocketChattingBinding>(R.layout.activity_socket_chatting) {
 
-    private val viewModel : ChattingViewModel by viewModels()
+    private val viewModel: ChattingViewModel by viewModels()
+    private lateinit var adapter: ChattingMultiHolderAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding.lifecycleOwner = this
+        onBind()
+        collectList()
+
+    }
+
+    private fun collectList() {
+        viewModel.chattingList.flowWithLifecycle(lifecycle).onEach {
+            when (it) {
+                is ApiResult.Idle -> {
+                    Timber.e("Init!!!!!")
+                }
+                is ApiResult.Loading -> {
+                    Timber.e("Loading!!!!!!!!")
+                }
+                is ApiResult.Success -> {
+                    Timber.e("Success!!!!!!!!")
+                    if (it.datas.isNotEmpty()) {
+                        adapter.submitList(it.datas)
+                    }
+                }
+                is ApiResult.Failure -> {
+                    Timber.e("Failure!!!!!!!! ${it.throwable}")
+                }
+            }
+        }.launchIn(lifecycleScope)
+    }
+
+    private fun onBind() {
+        adapter = ChattingMultiHolderAdapter()
+        binding.rcChattingList.adapter = adapter
     }
 }
