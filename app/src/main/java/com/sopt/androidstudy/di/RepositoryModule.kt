9dee35520e4002a35307dd4a2f95af7c12a61990
minho <my_alsgh@naver.com>
2022-10-04 02:48:07 +0900

    [Feat] Dependency Inject Repository, Service Service Can Inject @Binds

--- app/src/main/java/com/sopt/androidstudy/di/RepositoryModule.kt
+++ app/src/main/java/com/sopt/androidstudy/di/RepositoryModule.kt
@@ -2,8 +2,10 @@ package com.sopt.androidstudy.di
 
 import com.sopt.androidstudy.data.repository.ChattingRepositoyImpl
 import com.sopt.androidstudy.data.repository.FriendRepositoryImpl
+import com.sopt.androidstudy.data.repository.LottoRepositoryImpl
 import com.sopt.androidstudy.domain.repository.ChattingRepository
 import com.sopt.androidstudy.domain.repository.FriendRepository
+import com.sopt.androidstudy.domain.repository.LottoRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -21,4 +23,8 @@ abstract class RepositoryModule {
     @Binds
     @ViewModelScoped
     abstract fun bindToChattingRepository(chattingRepositoyImpl: ChattingRepositoyImpl):ChattingRepository
+
+    @Binds
+    @ViewModelScoped
+    abstract fun bindToLottoRepository(lottoRepositoryImpl: LottoRepositoryImpl):LottoRepository
 }
