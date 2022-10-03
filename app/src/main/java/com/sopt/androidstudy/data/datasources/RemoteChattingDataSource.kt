390281672e9f1e68e538e028ed451f043e75f9d4
minho <my_alsgh@naver.com>
2022-10-04 01:15:13 +0900

    [Chore] BaseResponse -> Response Type Use

--- app/src/main/java/com/sopt/androidstudy/data/datasources/RemoteChattingDataSource.kt
+++ app/src/main/java/com/sopt/androidstudy/data/datasources/RemoteChattingDataSource.kt
@@ -1,13 +1,12 @@
 package com.sopt.androidstudy.data.datasources
 
-import com.sopt.androidstudy.data.model.chatting.ChatDto
 import com.sopt.androidstudy.data.model.chatting.Messages
 import com.sopt.androidstudy.data.remote.chatting.ChattingService
-import com.sopt.androidstudy.data.util.BaseResponse
+import retrofit2.Response
 import javax.inject.Inject
 
 class RemoteChattingDataSource @Inject constructor(private val chattingService: ChattingService) {
 
-    suspend fun getChattingList(roomId: String): BaseResponse<Messages> =
+    suspend fun getChattingList(roomId: String): Response<Messages> =
         chattingService.getChattingList(roomId)
 }
