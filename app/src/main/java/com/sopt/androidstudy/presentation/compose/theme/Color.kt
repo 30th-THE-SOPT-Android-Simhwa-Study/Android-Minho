0414db8a61094e03a5aff72d05eb94680f43be1a
minho <my_alsgh@naver.com>
2022-11-28 03:41:56 +0900

    [Chore] Compose Initial Setting

---
+++ app/src/main/java/com/sopt/androidstudy/presentation/compose/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.sopt.androidstudy.presentation.compose.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Green500 = Color(0xFF62EC99)
+val Blue500 = Color(0xFF5DADEC)
+val Pink500 = Color(0xFFF282C5)
+val Orange500 = Color(0xFFFFC42C)
