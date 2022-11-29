6c72bdd28801b7a4afbe856e57eece13d6057c6b
minho <my_alsgh@naver.com>
2022-11-30 02:59:38 +0900

    [Feat] Todo List Item Create

--- app/src/main/java/com/sopt/androidstudy/presentation/compose/component/ToDoItem.kt
+++ app/src/main/java/com/sopt/androidstudy/presentation/compose/component/ToDoItem.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,20 +15,26 @@ import androidx.compose.ui.unit.dp
 import com.sopt.androidstudy.presentation.compose.model.ToDoTask
 
 @Composable
-fun ToDoList(toDoTask: ToDoTask) {
+fun ToDoItem(toDoTask: ToDoTask) {
     Column(Modifier.padding(16.dp, 5.dp)) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             Image(
                 painter = painterResource(id = toDoTask.type.res),
                 contentDescription = null,
+                modifier = Modifier.size(48.dp)
             )
             Column(
                 horizontalAlignment = Alignment.Start,
-                modifier = Modifier.padding(16.dp, 0.dp, 0.dp, 0.dp)
+                modifier = Modifier
+                    .padding(16.dp, 0.dp, 0.dp, 0.dp)
+                    .weight(1F)
             ) {
                 Text(text = toDoTask.type.info, color = Color.Black)
                 Text(text = toDoTask.content, color = Color.Black)
-                Text(text = toDoTask.time, color = Color.Black)
+                Text(
+                    text = (if (toDoTask.time.hour / 12 == 1) "오후 " + toDoTask.time.hour % 12 + "시" else "오전 " + toDoTask.time.hour + "시").toString(),
+                    color = Color.Black
+                )
             }
         }
     }
