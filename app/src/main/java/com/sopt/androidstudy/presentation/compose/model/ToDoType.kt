452b6e72061f0a0ce234e1e5f507d98af55f197c
minho <my_alsgh@naver.com>
2022-11-30 03:30:27 +0900

    [Chore] Todo Type Modify

--- app/src/main/java/com/sopt/androidstudy/presentation/compose/model/ToDoType.kt
+++ app/src/main/java/com/sopt/androidstudy/presentation/compose/model/ToDoType.kt
@@ -1,11 +1,16 @@
 package com.sopt.androidstudy.presentation.compose.model
 
 import androidx.annotation.DrawableRes
+import androidx.compose.ui.graphics.Color
 import com.sopt.androidstudy.R
+import com.sopt.androidstudy.presentation.compose.theme.Blue500
+import com.sopt.androidstudy.presentation.compose.theme.Green500
+import com.sopt.androidstudy.presentation.compose.theme.Orange500
+import com.sopt.androidstudy.presentation.compose.theme.Pink500
 
-enum class ToDoType(@DrawableRes val res: Int, val info: String) {
-    HANG_OUT(R.drawable.ic_hang_out, "약속"),
-    STUDY(R.drawable.ic_study, "스터디"),
-    EXERCISE(R.drawable.ic_exercise, "운동"),
-    MEETING(R.drawable.ic_meeting, "미팅")
+enum class ToDoType(@DrawableRes val res: Int, val info: String, val color: Color) {
+    HANG_OUT(R.drawable.ic_hang_out, "약속", Green500),
+    STUDY(R.drawable.ic_study, "스터디", Blue500),
+    EXERCISE(R.drawable.ic_exercise, "운동", Pink500),
+    MEETING(R.drawable.ic_meeting, "미팅", Orange500)
 }
